91b671e84e7531c1046426214b41660c00d9eda2
keavenymalachy <malachyk100@gmail.com>
2022-07-26 23:32:53 +0100

    Finished Home section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,48 +1,68 @@
-import React, { useState } from 'react'
-import {FaBars, FaTimes} from 'react-icons/fa'
-import Logo from '../assets/logo.png'
-import { useState } from 'react'
+import React, { useState } from "react";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsFillPersonLinesFill } from 'react-icons/bs'
+import Logo from "../assets/logo.png";
 
 const Navbar = () => {
-  
-  const {nav, setNav} = useState(false)
-  const handleClick = () => setNav(!nav)
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-6 bg-[#0a192f] text-gray-300'>
-        
-        <div>
-          <img src={Logo} alt="Logo" style={{width:'75px'}}/>
-        </div>
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-6 bg-[#0a192f] text-gray-300">
+      <div>
+        <img src={Logo} alt="Logo" style={{ width: "75px" }} />
+      </div>
 
-        {/* Menu */}
-          <ul className='hidden md:flex'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Experience</li>
-            <li>Work</li>
-            <li>Contact</li>
-          </ul>
+      {/* Menu */}
+      <ul className="hidden md:flex">
+        <li>Home</li>
+        <li>About</li>
+        <li>Experience</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
 
-        {/* Hamburger */}
-        <div onClick={handleClick} className='md:hidden z-10'>
-          <FaBars />
-        </div>
+      {/* Hamburger */}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
 
-        {/* Mobile menu */}
-        <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-            <li className='py-6 text-4xl'>Home</li>
-            <li className='py-6 text-4xl'>About</li>
-            <li className='py-6 text-4xl'>Experience</li>
-            <li className='py-6 text-4xl'>Work</li>
-            <li className='py-6 text-4xl'>Contact</li>
+      {/* Mobile menu */}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Experience</li>
+        <li className="py-6 text-4xl">Work</li>
+        <li className="py-6 text-4xl">Contact</li>
+      </ul>
+
+      {/* social icons */}
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 rounded">
+            <a className="flex  justify-between items-center w-full text-grey-300"
+            href="https://www.linkedin.com/in/malachy-keaveny-3229641a4/">
+              LinkedIn <FaLinkedin size={30}/>
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333] rounded">
+            <a className="flex  justify-between items-center w-full text-grey-300"
+            href="https://github.com/keavenymalachy">
+              Github <FaGithub size={30}/>
+            </a>
+          </li>
         </ul>
-
-        {/* social icons */}
-        <div className='hidden'></div>
+      </div>
 
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
