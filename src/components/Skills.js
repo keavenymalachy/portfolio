949fb14ea5f78a995ffb56dfc50c1d3485192c1e
Malachy <malachyk100@gmail.com>
2022-08-13 00:28:42 +0100

    Started skills page

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import HTML from '../assets/html.png';
+import CSS from '../assets/css.png';
+import JavaScript from '../assets/javascript.png';
+import ReactImg from '../assets/react.png';
+import Node from '../assets/node.png';
+import FireBase from '../assets/firebase.png';
+import AWS from '../assets/aws.png';
+import GitHub from '../assets/github.png';
+import Tailwind from '../assets/tailwind.png';
+import Mongo from '../assets/mongo.png';
 
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <div name='skills' className='bg-[#0a192f] text-gray-300'>
+      {/* Container */}
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+          <div>
+            <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Experience</p>
+            <p className='py-4'>// These are the technologies I've worked with</p>
+          </div>
+
+          <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+            <div className='shadow-md shadow-[#040c16]'>
+              <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
+              <p>HTML</p>
+            </div>
+          </div>
+        </div>
+    </div>
   )
 }
 
