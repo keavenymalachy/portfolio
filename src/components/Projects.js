28c99f13c36bd8485caf1b6902b799ac5269be00
Malachy <malachyk100@gmail.com>
2022-08-15 10:17:21 +0100

    Added skills and projects pages

---
+++ src/components/Projects.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Workimg from '../assets/workImg.jpeg'
+import realEstate from '../assets/realestate.jpg'
+
+const Projects = () => {
+    return (
+        <div name='projects' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+            {/* Container */}
+            <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+                <div className='pb-8'>
+                    <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Work</p>
+                    <p className='py-6'>Check out some of my recent work</p>
+                </div>
+
+                {/* Container for projects*/}
+                <div style={{backgroundImage: `url(${Workimg})`}}
+                 className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+                    {/* Specific card items*/}
+                    <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                        
+                        {/* Hover effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                React JS Application
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href=''>
+                                    <button>Demo</button>
+                                </a>
+                                <a href=''>
+                                    <button>Code</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Projects
