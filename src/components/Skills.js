54d1953b711e855b8c4e8363529c35f611b51ba0
keavenymalachy <malachyk100@gmail.com>
2022-08-15 16:35:09 +0100

    Fixed bug

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,7 +3,6 @@ import HTML from '../assets/html.png';
 import CSS from '../assets/css.png';
 import JavaScript from '../assets/javascript.png';
 import ReactImg from '../assets/react.png';
-import Node from '../assets/node.png';
 import Tailwind from '../assets/tailwind.png';
 
 
@@ -40,10 +39,6 @@ const Skills = () => {
             <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
               <img className='w-20 mx-auto' src={CSS} alt="HTML icon" />
               <p className='my-4'>CSS</p>
-            </div>
-            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-              <img className='w-20 mx-auto' src={Node} alt="HTML icon" />
-              <p className='my-4'>NodeJS</p>
             </div>
 
           </div>
